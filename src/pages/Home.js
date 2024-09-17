20e4da46540acb372b29f9752abb019929a52ca9
Mason <mdpafford2015@gmail.com>
2024-09-17 18:26:27 -0500

    completed assignment

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
-import { useState, useEffect } from "react";
+// Home.js
+import { useState, useEffect } from "react"
 import UserCard from "../components/UserCard";
+import NavBar from "../components/NavBar";
 
 function Home() {
   const [users, setUsers] = useState([])
@@ -8,9 +10,9 @@ function Home() {
     fetch("http://localhost:4000/users")
       .then(r => r.json())
       .then(data => setUsers(data))
-      .catch(error => console.error(error));
-  }, []);
-  
+      .catch(error => console.error(error))
+  }, [])
+
   const userList = users.map(user =>{
     return <UserCard key={user.id} user={user}/>
   });
@@ -18,7 +20,7 @@ function Home() {
   return (
     <>
       <header>
-        {/* place NavBar here */}
+        <NavBar />
       </header>
       <main>
         <h1>Home!</h1>
